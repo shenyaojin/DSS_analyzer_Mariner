02d22aeccbcd3cc80204e18940ab6b4aec0e4743
Shenyao Jin <106902466+shenyaojin@users.noreply.github.com>
2023-11-16 14:14:32 -0700

    Update event_analysis_tools.py

--- event_analysis_tools.py
+++ event_analysis_tools.py
@@ -59,3 +59,60 @@ def cross_correlation(a, b):
     corr_result = np.fft.ifft(fft_result)
 
     return corr_result
+
+import numpy as np
+
+def slippage_removal(trc, detect_thres=3, data_point_removal=4, sm_N=2, abs_thres=False, local_std_N=500, is_interp=True):
+    # Calculate strain rate
+    trc_diff = np.diff(trc)
+
+    # Detect slippage events
+    if abs_thres:
+        ind = np.abs(trc_diff) > detect_thres
+    else:
+        ind = np.abs(trc_diff) > detect_thres * np.std(trc_diff)
+    ind = np.where(ind)[0]
+
+    if len(ind) > len(trc) * 0.2:
+        return trc
+
+    # For each slippage, check local variance again
+    if not abs_thres:
+        ind_to_remove = []
+        for i in range(len(ind) - 1):
+            bgind = max(0, ind[i] - local_std_N // 2)
+            edind = min(ind[i] + local_std_N // 2, len(trc_diff))
+
+            local_std = np.std(trc_diff[bgind:edind])
+            if np.abs(trc_diff[ind[i]]) < detect_thres * local_std:
+                ind_to_remove.append(i)
+        
+        ind = np.delete(ind, ind_to_remove)
+
+    # Remove data points after slippage events
+    new_ind = []
+    for i in ind:
+        new_ind.extend(range(i, min(i + data_point_removal, len(trc_diff))))
+
+    if len(new_ind) == 0:
+        return trc
+
+    # Perform interpolation
+    good_ind = np.ones(len(trc_diff), dtype=bool)
+    good_ind[new_ind] = False
+    x = np.arange(len(trc_diff))
+    good_trc_diff = trc_diff[good_ind].copy()
+
+    # Apply smoothing
+    if sm_N > 1:
+        good_trc_diff = np.convolve(good_trc_diff, np.ones(sm_N) / sm_N, mode='same')
+
+    # Interpolation
+    if is_interp:
+        trc_diff[~good_ind] = np.interp(x[~good_ind], x[good_ind], good_trc_diff)
+    else:
+        trc_diff[~good_ind] = 0
+
+    # Change back to strain change
+    trc_cor = np.concatenate(([trc[0]], trc[0] + np.cumsum(trc_diff)))
+    return trc_cor
