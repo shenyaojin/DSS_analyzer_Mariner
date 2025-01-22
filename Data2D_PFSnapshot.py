d78acaa99a83484d479b98f7bad87accf6af9a7b
Shenyao Jin <106902466+shenyaojin@users.noreply.github.com>
2025-01-22 15:09:21 -0700

    Update snapshot/062

--- Data2D_PFSnapshot.py
+++ Data2D_PFSnapshot.py
@@ -1,1 +1,12 @@
 # Pass the data from PDS to the DSS analyzer.
+
+import numpy as np
+from datetime import datetime, timedelta
+
+class Data2D_PFSnapshot:
+    def __init__(self):
+        self.data = None
+        self.taxis = None
+        self.daxis = None
+        self.start_time = None
+        self.history = None
