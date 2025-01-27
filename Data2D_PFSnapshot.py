c173a592db49e81ad785e113ccb670960ce5a718
Shenyao Jin <106902466+shenyaojin@users.noreply.github.com>
2025-01-26 21:35:04 -0700

    Update Data2D_PFSnapshot.py

--- Data2D_PFSnapshot.py
+++ Data2D_PFSnapshot.py
@@ -1,4 +1,6 @@
 # Pass the data from PDS to the DSS analyzer.
+# Analyzer of the synthetic data from the PDS. Since it has no time stamp info
+# So I need a new class to handle the data. And that's why I write this class.
 
 import numpy as np
 from datetime import datetime, timedelta
@@ -11,4 +13,15 @@ class Data2D_PFSnapshot:
         self.start_time = None
         self.history = None
 
-    def load_snapshot
+    def load_snapshot(self, **kwargs):
+        # Load the file from data, or using the data directly.
+        if 'data' in kwargs:
+            self.data = kwargs['data']
+            # Also, set the time axis and the distance axis.
+            self.taxis = kwargs['taxis']
+            self.daxis = kwargs['daxis']
+        elif 'file' in kwargs:
+            self.data = np.load(kwargs['file'])
+        else:
+            print('No data is provided.')
+            return
