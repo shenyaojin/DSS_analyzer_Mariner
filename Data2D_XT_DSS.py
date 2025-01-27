cd4358aac16497c85629d32da7e2925e52b802c7
Shenyao Jin <106902466+shenyaojin@users.noreply.github.com>
2025-01-27 00:40:39 -0700

    Update dependencies of Data2D_XT_DSS.py

--- Data2D_XT_DSS.py
+++ Data2D_XT_DSS.py
@@ -4,7 +4,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime,timedelta
-from scipy.signal import medfilt2d, tukey
+from scipy.signal import medfilt2d
+from scipy.signal.windows import tukey
 import matplotlib.dates as mdates
 from dateutil.parser import parse
 from copy import copy
